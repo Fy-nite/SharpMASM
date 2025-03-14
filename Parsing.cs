174be59ed8d858fee53fffc7cd013e59326694e2
charliesan <charliesan@envs.net>
2025-03-14 18:00:44 +1100

    Add project files.

---
+++ Parsing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpMASM
+{
+
+    /*
+     * Parsing.cs
+     * 
+     * Description: Parsing class for the SharpMASM project.
+     *
+     * (C) Finite, 2025
+     */
+
+
+    /*
+     * Micro-Assembly has some great features like labels, comments, and instructions.
+     * this class helps parse out the instructions and labels from the source code.
+     * 
+     * labels aare defined by 
+     * lbl <name>
+     * 
+     * and are added to the label map.
+     * these are preprocess steps before the actual execution of each instruction.
+     * 
+     * we also have include statements
+     * #include "file"
+     * 
+     * lets say we want a stdio function right?
+     * #include "stdio.print"
+     * this will include the <root>/stdio/print.masm file
+     * 
+     */
+
+    // instruction class
+    public class Parsing
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SharpMasm;
+public class Entry
+{
+
+    public static void Main(string[] args)
+    {
+        
+    }
+}
